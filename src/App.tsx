03d8c81031cd26fe3282268f6474ebd51e2fc7d5
Lucy Shen <lucy.shen01@gmail.com>
2021-05-08 00:48:24 -0400

    finished technical requirements, localhost, loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -17,26 +17,7 @@ import { MovieSearch } from "./MovieSearch"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    {/* <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box> */}
     <MovieSearch />
   </ChakraProvider>
 )
+ 
--- src/MovieSearch.tsx
+++ src/MovieSearch.tsx
@@ -1,6 +1,6 @@
-import React, { FC, useState, useEffect } from "react";
-import { useDebounce } from "use-debounce";
+import React, { FC, useState, useEffect, useMemo } from "react";
 import axios from "axios";
+import { useDebounce } from "use-debounce";
 
 import {
   Box,
@@ -10,6 +10,11 @@ import {
   Container,
   Heading,
   HStack,
+  Button,
+  Center,
+  Spinner,
+  Alert,
+  AlertIcon,
 } from "@chakra-ui/react";
 
 type MovieInfo = {
@@ -19,15 +24,19 @@ type MovieInfo = {
   imdbID: string;
 };
 
+const PAST_MOVIE_NOMS_KEY = "PAST_MOVIE_NOMINATIONS";
+
 export const MovieSearch: FC = () => {
   const [query, setQuery] = useState("");
   const [deQuery] = useDebounce(query, 1000);
   const [loading, setLoading] = useState(true);
 
-  // const handleChange = (event) => setQuery(event.target.value);
-
-  // TODO: Add 'searchMovies' state.
-  // TODO: Add 'nomineeMovies' state.
+  const initialMovieNoms = useMemo<MovieInfo[]>(() => {
+    const value = localStorage.getItem(PAST_MOVIE_NOMS_KEY);
+    return value ? JSON.parse(value) : [];
+  }, []);
+  const [movieResults, setMovieResults] = useState<MovieInfo[]>([]);
+  const [movieNoms, setMovieNoms] = useState<MovieInfo[]>(initialMovieNoms);
 
   useEffect(() => {
     const search = async () => {
@@ -37,43 +46,139 @@ export const MovieSearch: FC = () => {
           params: { apikey: "22aa1b2", s: deQuery },
         });
         if (data.Search) {
-          // TODO: setSearchMovies(data.Search as MovieInfo[]);
-          // return data.Search as MovieInfo[];
-          console.log(data.Search);
-        } else {
-          alert(`Unexpected error from API: {data.Error.toString()}`);
+          setMovieResults(data.Search);
+        } else if (data.Error) {
+          if (data.Error === "Movie not found!") {
+          } else {
+            alert(`Unexpected error from API: ${data.Error.toString()}`);
+          }
         }
       } catch (error) {
-        console.error("Unexpected network error!", { error });
+        alert(`Unexpected network error: ${error.toString()}`);
       } finally {
         setLoading(false);
       }
     };
-    search();
+    if (deQuery.length > 2) {
+      search();
+    } else {
+      setMovieResults([]);
+    }
   }, [deQuery]);
 
+  useEffect(() => {
+    localStorage.setItem(PAST_MOVIE_NOMS_KEY, JSON.stringify(movieNoms));
+  }, [movieNoms]);
+
+  console.log(movieResults);
+
+  const renderMessage = () => {
+    if (loading) {
+      return <Spinner />;
+    }
+    if (!query) {
+      return <Text>Enter a movie name to get started!</Text>;
+    }
+    if (query.length < 3) {
+      return <Text>Movie name must be at least 3 characters</Text>;
+    }
+    return <Text>Movie not found</Text>;
+  };
+
   return (
-    <Container py={12}>
+    <Container py={12} maxW="container.lg">
       <VStack align="stretch" spacing={4}>
-        <Box>
-          <Heading>Some title</Heading>
-          <Text>Search for a movie to get started</Text>
-        </Box>
-        <HStack>
-          <Input
-            placeholder="Search"
-            value={query}
-            onChange={(event) => setQuery(event.target.value)}
-          />
-          {/* <Button /> */}
-        </HStack>
-        <HStack align="stretch" spacing={4}>
-          <Box shadow="md" borderWidth="1px" flex="1" borderRadius="md">
-            <Text fontSize="3xl">Results</Text>
-          </Box>
-          <Box shadow="md" borderWidth="1px" flex="1" borderRadius="md">
-            <Text fontSize="3xl">Nominations</Text>
-          </Box>
+        <VStack align="stretch">
+          <Heading size="2xl">The Shoppies</Heading>
+          <Text fontSize="lg">
+            Nominate 5 movies. Your nominations will be saved if you leave this
+            page.
+          </Text>
+        </VStack>
+        <Input
+          placeholder="Search"
+          value={query}
+          size="lg"
+          onChange={(event) => setQuery(event.target.value)}
+        />
+        {movieNoms.length >= 5 && (
+          <Alert status="success" fontSize="lg">
+            <AlertIcon />5 nominees selected!
+          </Alert>
+        )}
+        <HStack align="stretch" minH={96}>
+          <VStack
+            align="stretch"
+            flex={1}
+            spacing={4}
+            shadow="md"
+            borderWidth="1px"
+            borderRadius="md"
+            p={4}
+          >
+            <Heading fontSize="3xl">Results</Heading>
+            {movieResults.length > 0 ? (
+              movieResults.map((item) => {
+                return (
+                  <HStack>
+                    <VStack align="stretch" flex={1} spacing={0}>
+                      <Text fontSize="xl" fontWeight="semibold">
+                        {item.Title}
+                      </Text>
+                      <Text fontSize="lg">{item.Year}</Text>
+                    </VStack>
+                    <Button
+                      onClick={() => {
+                        setMovieNoms([...movieNoms, item]);
+                      }}
+                      isDisabled={
+                        movieNoms.some((nom) => {
+                          return nom.imdbID === item.imdbID;
+                        }) || movieNoms.length >= 5
+                      }
+                    >
+                      Nominate
+                    </Button>
+                  </HStack>
+                );
+              })
+            ) : (
+              <Center fontSize="lg">{renderMessage()}</Center>
+            )}
+          </VStack>
+          <VStack
+            align="stretch"
+            flex={1}
+            spacing={4}
+            shadow="md"
+            borderWidth="1px"
+            borderRadius="md"
+            p={4}
+          >
+            <Heading fontSize="3xl">Nominations</Heading>
+            {movieNoms.map((item) => {
+              return (
+                <HStack>
+                  <VStack align="stretch" flex={1} spacing={0}>
+                    <Text fontSize="xl" fontWeight="semibold">
+                      {item.Title}
+                    </Text>
+                    <Text fontSize="lg">{item.Year}</Text>
+                  </VStack>
+                  <Button
+                    onClick={() => {
+                      const noms = movieNoms.filter((oneOfTheMoviesInNom) => {
+                        return oneOfTheMoviesInNom.imdbID !== item.imdbID;
+                      });
+                      setMovieNoms(noms);
+                    }}
+                  >
+                    Remove
+                  </Button>
+                </HStack>
+              );
+            })}
+          </VStack>
         </HStack>
       </VStack>
     </Container>
